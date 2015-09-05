186432b89be8558192f5b2385b2839bd8b4b6644
ebolatumor <komlosi.attila@gmail.com>
2015-09-06 00:27:58 +0200

    remove old files

--- tests/hello_test.js
+++
@@ -1,8 +0,0 @@
-var assert = chai.assert;
-mocha.setup('tdd');
-
-suite('hello test with mocha', function(){
-	test('should greet Roger', function(){
-		assert.isTrue(hello('Roger') === 'hello Roger');
-	});
-});
